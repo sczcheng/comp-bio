0ccc7386dca493ec43614c3b3cf2704e258ad12e
Sophia Cheng <socheng@hmc.edu>
2020-10-21 13:21:58 +0800

    finished hapCounts

--- hw01/hapCounts.py
+++ hw01/hapCounts.py
@@ -1,22 +1,41 @@
-fin_data = open("hapCounts/sample.tsv")
+import pprint
 
-#hapCounts/sample.tsv
-#fin-2.136445439-136692143.tsv
+def readHaps(myData):
+    data = open(myData)
 
-first_line = fin_data.readline()
-num_tuples = (len(first_line.split()) - 5) * 2 
+    #hapCounts/sample.tsv
+    #fin-2.136445439-136692143.tsv
 
-final_list = [[] for _ in range(num_tuples)]
+    first_line = data.readline()
+    num_tuples = (len(first_line.split()) - 5) * 2 
+
+    final_list = [[] for _ in range(num_tuples)]
 
 
-for line in fin_data:
-    cells = line.split()[5:]
+    for line in data:
+        cells = line.split()[5:]
 
-    for i, cell in enumerate(cells):
-        first, second = cell.split(',')
-        final_list[i*2].append(first)
-        final_list[2*i+1].append(second)
+        for i, cell in enumerate(cells):
+            first, second = cell.split(',')
+            final_list[i*2].append(first)
+            final_list[2*i+1].append(second)
 
-tuple_list = [tuple(list) for list in final_list]
+    tuple_list = [tuple(list) for list in final_list]
+    return tuple_list
 
-print(tuple_list)
+print(readHaps("hapCounts/sample.tsv"))
+
+
+
+def hapCounts(hapDataL):
+    hapCounts_Dict = {}
+
+    for tup in hapDataL:
+        if tup in hapCounts_Dict:
+            hapCounts_Dict[tup] += 1
+        else:
+            hapCounts_Dict[tup] = 1
+
+    return hapCounts_Dict
+
+#pprint.pprint(hapCounts(tuple_list))
